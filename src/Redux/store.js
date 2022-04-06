88ac9be3201d8d1779437b999e3277429140b9e2
Iryna Shandar <iryna.shandar@gmail.com>
2022-04-07 02:43:15 +0300

    store

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -10,7 +10,6 @@ import {
   REGISTER,
 } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-import { todosReducer } from "./todos";
 import { authReducer } from "./auth";
 
 const middleware = [
@@ -30,10 +29,30 @@ const authPersistConfig = {
 export const store = configureStore({
   reducer: {
     auth: persistReducer(authPersistConfig, authReducer),
-    todos: todosReducer,
   },
   middleware,
   devTools: process.env.NODE_ENV === "development",
 });
 
 export const persistor = persistStore(store);
+
+
+
+////////////////////////////////////
+// import { configureStore } from '@reduxjs/toolkit';
+// import { contactsApi } from '../components/API/contacts.API';
+// import { filter } from './contacts/contacts-reducer';
+
+// export const store = configureStore({
+//   reducer: {
+//     filter,
+//     [contactsApi.reducerPath]: contactsApi.reducer,
+//   },
+
+//   middleware: getDefaultMiddleware => [
+//     ...getDefaultMiddleware(),
+//     contactsApi.middleware,
+//   ],
+//   devTools: process.env.NODE_ENV === 'development',
+// });
+/////////////////////////////////////
