e7d2dad15897e3bcba50cd0f1ea164b3a564a0b2
Iryna Shandar <iryna.shandar@gmail.com>
2022-04-07 14:19:29 +0300

    delete

--- src/redux/contacts/contacts-reducer.js
+++
@@ -1,6 +0,0 @@
-import { createReducer } from "@reduxjs/toolkit";
-import { changeFilter } from "./contacts-actions";
-
-export const filter = createReducer("", {
-  [changeFilter]: (_, { payload }) => payload,
-});
