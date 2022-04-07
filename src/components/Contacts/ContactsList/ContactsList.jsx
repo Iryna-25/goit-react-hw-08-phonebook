a3658734e19ed088ec644fe3bd5c901bba993ddb
Iryna Shandar <iryna.shandar@gmail.com>
2022-04-07 14:21:45 +0300

    addining

---
+++ src/components/Contacts/ContactsList/ContactsList.jsx
@@ -0,0 +1,68 @@
+import { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import toast, { Toaster } from 'react-hot-toast';
+import { List, Item, DeleteButton, Total } from './ContactsList.styled';
+import { useSelector } from 'react-redux';
+import {
+    useGetContactsQuery,
+    useDeleteContactMutation,
+} from '../../API/contacts.API';
+import { getFilter } from '../../../redux/contacts/contacts-selector';
+
+const ContactsList = () => {
+    const filter = useSelector(getFilter);
+    const { data: contacts, error } = useGetContactsQuery();
+    const [onDelete] = useDeleteContactMutation();
+
+    const normalizedFilter = filter
+        // .toLowerCase()
+        ;
+    let FilContacts = [];
+
+    if (contacts) {
+        FilContacts = contacts.filter(({ name }) =>
+            name.toLowerCase().includes(normalizedFilter),
+        );
+    }
+    useEffect(() => {
+        if (error) {
+            toast.error(`Server error`);
+        }
+    }, [error]);
+
+    return (
+        <>
+            {error && <Toaster />}
+            {!error && contacts && (
+                <List>
+                    <Total>Total contacts: {FilContacts.length}</Total>
+                    {FilContacts.map(({ id, name, number }) => (
+                        <Item key={id}>
+                            {name} : {number}
+                            {
+                                <DeleteButton
+                                    type="button"
+                                    name="delete"
+                                    onClick={() => onDelete(id)}
+                                >
+                                    Delete
+                                </DeleteButton>
+                            }
+                        </Item>
+                    ))}
+                </List>
+            )}
+        </>
+    );
+};
+
+ContactsList.propTypes = {
+    contacts: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            name: PropTypes.string.isRequired,
+            number: PropTypes.string.isRequired,
+        }),
+    ),
+};
+export default ContactsList;
